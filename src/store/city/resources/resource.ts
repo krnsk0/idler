d51299c3b4df175a5dc2551cfc04389354551464
krnsk0 <jkurinsky@gmail.com>
2022-06-25 14:51:23 -0400

    extract decorators to separate class

--- src/store/city/resources/resource.ts
+++ src/store/city/resources/resource.ts
@@ -1,19 +1,35 @@
-import { idProp, Model, tProp, types, modelAction } from 'mobx-keystone';
+import {
+  idProp,
+  Model,
+  tProp,
+  types,
+  modelAction,
+  decoratedModel,
+} from 'mobx-keystone';
 
-export abstract class Resource extends Model({
+export abstract class _Resource extends Model({
   id: idProp,
   qty: tProp(types.number, 0),
 }) {
   abstract displayName: string;
   abstract displayColor: string;
 
-  // @modelAction
   increase(qty: number): void {
     this.qty += qty;
   }
 
-  // @modelAction
   decrease(qty: number): void {
     this.qty -= qty;
   }
 }
+
+/**
+ * Needed because decorators do not work in abstract classses
+ * See https://mobx-keystone.js.org/class-models#usage-without-decorators
+ */
+export const Resource = decoratedModel(undefined, _Resource, {
+  increase: modelAction,
+  decrease: modelAction,
+});
+
+type Resource = _Resource;
