51dfe4257978f3b50a483d01499601d51c9b16dd
krnsk0 <jkurinsky@gmail.com>
2022-07-04 12:31:59 -0400

    styling

--- src/sharedComponetns/borderContainer/BorderContainer.styles.ts
+++ src/sharedComponetns/borderContainer/BorderContainer.styles.ts
@@ -5,7 +5,7 @@ const titleOffset = '10px';
 export const styles = {
   container: css`
     border: 1px solid black;
-    padding: 8px 6px 6px 6px;
+    padding: 0.6em 0.4em 0.4em 0.4em;
     position: relative;
     top: ${titleOffset};
     height: calc(100% - ${titleOffset});
