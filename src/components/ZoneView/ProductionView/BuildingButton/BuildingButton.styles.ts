b16e674a7b7af98e1e25909b71d2bbf4f49e230f
krnsk0 <jkurinsky@gmail.com>
2022-07-04 13:46:23 -0400

    margin adjust

--- src/components/ZoneView/ProductionView/BuildingButton/BuildingButton.styles.ts
+++ src/components/ZoneView/ProductionView/BuildingButton/BuildingButton.styles.ts
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 export const styles = {
   buildingBox: css`
     border: 1px solid black;
-    margin: 0.1em;
+    margin: 0.3em 0em 0.3em 0em;
     padding: 0.2em;
     width: 180px;
     background: none;
