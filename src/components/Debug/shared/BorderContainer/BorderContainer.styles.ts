45f8bd6ab5472fa8bc244accaf1c37ccfa09f9c9
krnsk0 <jkurinsky@gmail.com>
2022-07-05 17:50:44 -0400

    style fixes

--- src/components/Debug/shared/BorderContainer/BorderContainer.styles.ts
+++ src/components/Debug/shared/BorderContainer/BorderContainer.styles.ts
@@ -6,6 +6,7 @@ export const styles = {
   container: css`
     border: 1px solid black;
     padding: 0.6em 0.4em 0.4em 0.4em;
+    margin-top: 0.5em;
     margin-bottom: 1em;
     position: relative;
     top: ${titleOffset};
