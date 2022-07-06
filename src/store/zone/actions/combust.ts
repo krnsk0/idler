bfad66f44792d170d754ccdf8f026284479bdc72
krnsk0 <jkurinsky@gmail.com>
2022-07-05 20:16:58 -0400

    better splash

--- src/store/zone/actions/combust.ts
+++ src/store/zone/actions/combust.ts
@@ -1,4 +1,4 @@
-import { model, ExtendedModel, modelAction } from 'mobx-keystone';
+import { model, ExtendedModel, modelAction, tProp, types } from 'mobx-keystone';
 import { ResourceNames } from '../resources/resourceNames';
 import { ActionNames } from './actionNames';
 import { BaseAction } from './baseAction';
@@ -6,13 +6,14 @@ import { BaseAction } from './baseAction';
 @model(ActionNames.COMBUST)
 export class Combust extends ExtendedModel(BaseAction, {}) {
   name = ActionNames.COMBUST;
-  displayName = 'combust biomass';
-  description = 'capture power from exothermic, self-perpetuating reaction';
-  duration = 3;
+  displayName = 'refuel generator';
+  description =
+    'generate electrical potential from exothermic, self-perpetuating reaction';
+  duration = 2;
   inputs = [
     {
       resource: ResourceNames.BIOMASS,
-      quantity: 3,
+      quantity: 1,
     },
   ];
   outputs = [];
