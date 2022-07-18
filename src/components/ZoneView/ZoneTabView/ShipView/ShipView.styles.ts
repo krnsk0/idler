0816f19aeb946aa4c0ff48cb00f85fe5be62bc81
krnsk0 <jkurinsky@gmail.com>
2022-07-17 21:37:50 -0400

    grid fix

--- src/components/ZoneView/ZoneTabView/ShipView/ShipView.styles.ts
+++ src/components/ZoneView/ZoneTabView/ShipView/ShipView.styles.ts
@@ -9,9 +9,13 @@ export const styles = {
   `,
   shipColumnWrap: css`
     width: 100%;
+    min-width: 374px;
+    min-height: 77px;
     display: grid;
     grid-template-rows: repeat(auto, auto);
     grid-template-columns: repeat(2, auto);
+    align-items: flex-start;
+    justify-content: flex-start;
     gap: 0.6em;
   `,
 };
