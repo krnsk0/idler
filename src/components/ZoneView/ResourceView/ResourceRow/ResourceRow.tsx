b289ab8eaa315e895a6896075283110fa5bb20b3
krnsk0 <jkurinsky@gmail.com>
2022-07-24 18:04:23 -0400

    empty thumbnails in resource rows

--- src/components/ZoneView/ResourceView/ResourceRow/ResourceRow.tsx
+++ src/components/ZoneView/ResourceView/ResourceRow/ResourceRow.tsx
@@ -1,19 +1,35 @@
 import { observer } from 'mobx-react-lite';
+import { useRef } from 'react';
 import { BaseResource } from '../../../../store/zone/resources/baseResource';
 import { formatNumber } from '../../../../utils/formatNumber';
+import Tooltip, {
+  TooltipDivider,
+  TooltipText,
+} from '../../../shared/Tooltip/Tooltip';
 import { styles } from './ResourceRow.styles';
 
 interface ResourceRowProps {
   resource: BaseResource;
 }
 const ResourceRow = ({ resource }: ResourceRowProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
   const displayFudge = 0.000001;
   const showDelta =
     resource.estimatedRate > displayFudge ||
     resource.estimatedRate < -displayFudge;
 
   return (
-    <div css={styles.resourceRow} key={resource.name}>
+    <div ref={containerRef} css={styles.resourceRow} key={resource.name}>
+      <Tooltip
+        containerRef={containerRef}
+        tooltipTop={20}
+        tooltipLeft={10}
+        width={240}
+      >
+        <TooltipDivider text="consumption" />
+        <TooltipDivider text="production" />
+        <TooltipDivider text="storage" />
+      </Tooltip>
       <span css={styles.displayName}>{resource.displayName}</span>
       {showDelta && (
         <span css={styles.quantityPerSecond}>
