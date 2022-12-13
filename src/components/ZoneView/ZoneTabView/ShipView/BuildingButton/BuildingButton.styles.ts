eced0f23f8249370b419562e265facb49cc08cf4
jon kurinsky <jkurinsky@fastmail.com>
2022-12-12 19:48:05 -0500

    remove border

--- src/components/ZoneView/ZoneTabView/ShipView/BuildingButton/BuildingButton.styles.ts
+++ src/components/ZoneView/ZoneTabView/ShipView/BuildingButton/BuildingButton.styles.ts
@@ -1,5 +1,4 @@
 import { css } from '@emotion/react';
-import { colors } from '../../../../../globalStyles';
 
 export const styles = {
   onOff: css`
@@ -7,7 +6,6 @@ export const styles = {
     right: 0.5em;
     top: 0;
     height: 100%;
-    border-left: 1px solid ${colors.black};
     padding-left: 0.5em;
   `,
 };
