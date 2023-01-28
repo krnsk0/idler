af533ab6c932491d78e3254e4d9e3504a6dec543
Jonathan Kurinsky <jkurinsky@tubi.tv>
2023-01-28 14:41:26 -0500

    generators do not dismantle generate action

--- src/store/tech/electromagnetism.ts
+++ src/store/tech/electromagnetism.ts
@@ -1,6 +1,5 @@
 import { model, ExtendedModel } from 'mobx-keystone';
 import { getGame } from '../game';
-import { ActionNames } from '../zone/actions/actionNames';
 import { BuildingNames } from '../zone/buildings/buildingNames';
 import { ResourceNames } from '../zone/resources/resourceNames';
 import { BaseTech } from './baseTech';
@@ -10,14 +9,13 @@ import { TechNames } from './techNames';
 export class Electromagnetism extends ExtendedModel(BaseTech, {}) {
   name = TechNames.ELECTROMAGNETISM;
   displayName = 'electromagnetism';
-  description =
-    'dismantle ship’s generator and study its design, allowing the construction of additional generators';
+  description = 'allow the construction of additional generators';
   powerCost = 25;
   transientUnlockCheck = () =>
     getGame(this).initialZone.resources[ResourceNames.ALLOY].quantity >= 5;
   techUnlocked = [];
   actionsUnlocked = [];
-  actionsRelocked = [ActionNames.GENERATE];
+  actionsRelocked = [];
   jobsUnlocked = [];
   buildingsUnlocked = [BuildingNames.DYNAMO];
 }
