23c8c4197f914714ba218f7e98d4b44f338ffa98
krnsk0 <jkurinsky@fastmail.com>
2023-02-02 10:44:53 -0500

    combine components

--- src/components/ZoneView/ZoneView.styles.ts
+++ src/components/ZoneView/ZoneView.styles.ts
@@ -11,9 +11,40 @@ export const styles = {
     margin: 0.6em 0 0 0;
     height: 100%;
   `,
-
   zoneHeader: css`
     margin-top: 0.3em;
     margin-bottom: 0.5em;
+  `,
+  tabViewContainer: css`
+    display: flex;
+    flex-direction: column;
+    margin-top: 1em;
+  `,
+  tabRow: css`
+    font-size: 1.1em;
+    margin-top: 0.5em;
+    margin-bottom: 1.5em;
+  `,
+  separator: css`
+    margin-left: 0.5em;
+    padding-left: 0.5em;
+    border-left: 1px solid black;
+  `,
+  tabButton: (selected: boolean) => css`
+    border: none;
+    margin: 0;
+    padding: 0;
+    background: none;
+    color: inherit;
+    font: inherit;
+    text-decoration: ${selected ? 'underline' : 'none'};
+    &:hover {
+      text-decoration: underline;
+    }
+    font-size: 1em;
+    cursor: ${selected ? 'auto' : 'pointer'};
+  `,
+  tabContent: css`
+    padding-top: 0.5em;
   `,
 };
--- src/components/ZoneView/ZoneView.tsx
+++ src/components/ZoneView/ZoneView.tsx
@@ -2,17 +2,86 @@ import { observer } from 'mobx-react-lite';
 import { Zone } from '../../store/zone/zone';
 import { styles } from './ZoneView.styles';
 import ResourceView from './ResourceView/ResourceView';
-import ZoneTabView from './ZoneTabView/ZoneTabView';
+import { useState } from 'react';
+import ShipColonyView from './ZoneTabView/ShipView/ShipColonyView';
+import JobsView from './ZoneTabView/JobsView/JobsView';
 
 interface ZoneViewProps {
   zone: Zone;
 }
 
+enum ZoneTabNames {
+  ACTIONS = 'ACTIONS',
+  JOBS = 'JOBS',
+}
+
+const TabButton = ({
+  text,
+  selectedTab,
+  setSelectedTab,
+  tabName,
+}: {
+  text: string;
+  selectedTab: ZoneTabNames;
+  setSelectedTab: React.Dispatch<React.SetStateAction<ZoneTabNames>>;
+  tabName: ZoneTabNames;
+}) => {
+  return (
+    <button
+      type="button"
+      onClick={() => setSelectedTab(tabName)}
+      css={styles.tabButton(selectedTab === tabName)}
+    >
+      {text}
+    </button>
+  );
+};
+
+const Separator = () => <span css={styles.separator} />;
+
 function ZoneView({ zone }: ZoneViewProps) {
+  const [selectedTab, setSelectedTab] = useState<ZoneTabNames>(
+    ZoneTabNames.ACTIONS,
+  );
+
   return (
     <div css={styles.zoneOuter} id="zone-outer">
       <h2 css={styles.zoneHeader}>{zone.name}</h2>
-      <ZoneTabView zone={zone} />
+      <div css={styles.tabViewContainer}>
+        <div css={styles.tabRow}>
+          {zone.jobs.unlocked && (
+            <>
+              <TabButton
+                text="colony"
+                tabName={ZoneTabNames.ACTIONS}
+                selectedTab={selectedTab}
+                setSelectedTab={setSelectedTab}
+              />
+              <Separator />
+              <TabButton
+                text={`jobs${
+                  zone.jobs.unassigned > 0 ? ` (${zone.jobs.unassigned})` : ''
+                }`}
+                tabName={ZoneTabNames.JOBS}
+                selectedTab={selectedTab}
+                setSelectedTab={setSelectedTab}
+              />
+            </>
+          )}
+        </div>
+        <div css={styles.tabContent} id="zone-view-tab-content">
+          {(() => {
+            switch (selectedTab) {
+              case ZoneTabNames.ACTIONS:
+                return <ShipColonyView zone={zone} />;
+              case ZoneTabNames.JOBS:
+                return <JobsView zone={zone} />;
+              default:
+                throw new Error('should not reach this case');
+            }
+          })()}
+        </div>
+      </div>
       <ResourceView zone={zone} />
     </div>
   );
