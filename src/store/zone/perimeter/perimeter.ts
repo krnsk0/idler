86e9db481951ffee7f364340ded5e06345816877
krnsk0 <jkurinsky@fastmail.com>
2023-02-26 17:23:41 -0500

    modal opening stuff

--- src/store/zone/perimeter/perimeter.ts
+++ src/store/zone/perimeter/perimeter.ts
@@ -1,5 +1,6 @@
 import { ExtendedModel, model, modelAction, tProp, types } from 'mobx-keystone';
 import { ZoneEntity } from '../zoneEntity';
+import { computed } from 'mobx';
 
 const TIME_TO_UNLOCK_AFTER_FIRST_DYNAMO_CONSTRUCTION = 5;
 
@@ -9,6 +10,10 @@ export class Perimeter extends ExtendedModel(ZoneEntity, {
    * Perimeter unlocks 10s after first dynamo is constructed
    */
   timeToUnlock: tProp(types.maybe(types.number), undefined),
+  /**
+   * Has the user closed the perimeter warning modal?
+   */
+  hasWarningModalBeenClosed: tProp(types.boolean, false),
 }) {
   transientUnlockCheck = () => {
     return this.timeToUnlock !== undefined && this.timeToUnlock <= 0;
@@ -22,6 +27,22 @@ export class Perimeter extends ExtendedModel(ZoneEntity, {
     if (this.timeToUnlock === undefined) {
       this.timeToUnlock = TIME_TO_UNLOCK_AFTER_FIRST_DYNAMO_CONSTRUCTION;
     }
+  }
+
+  /**
+   * Opens the warning modal when perimeter first unlocks
+   */
+  @computed
+  get isWarningModalOpen(): boolean {
+    return this.unlocked && !!this.hasWarningModalBeenClosed;
+  }
+
+  /**
+   * Closes the warning modal once and for all
+   */
+  @modelAction
+  closeWarningModal(): void {
+    this.hasWarningModalBeenClosed = true;
   }
 
   /**
