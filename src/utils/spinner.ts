3adcec28e7ebe648256406716c6a230afc9b1d33
krnsk0 <jkurinsky@fastmail.com>
2023-04-28 09:54:38 -0400

    change spinner direction

--- src/utils/spinner.ts
+++ src/utils/spinner.ts
@@ -1,10 +1,9 @@
 /**
  * Translates a time in seconds to a spinner using the characters
  * | / - \
- * where each character is shown for 100ms.
  */
 export function spinner(time: number): string {
-  const chars = ['|', '/', '-', '\\'];
+  const chars = ['|', '\\', '-', '/'];
   const index = Math.floor(time / 0.1) % chars.length;
   return chars[index];
 }
