7aae6ecfa8d411d215bc5f7622af41894c212e0a
krnsk0 <jkurinsky@fastmail.com>
2023-04-30 16:24:26 -0400

    costs

--- src/store/zone/perimeter/perimeter.ts
+++ src/store/zone/perimeter/perimeter.ts
@@ -7,6 +7,8 @@ import { PhaseMantis } from './enemies/phaseMantis';
 import { waveBuilder } from './enemies/utils/waveBuilder';
 import { statModifierFactory } from './enemies/utils/statModifierFactory';
 import { EnemyNames } from './enemies/enemyNames';
+import { PurchaseCost } from '../sharedTypes';
+import { ResourceNames } from '../resources/resourceNames';
 
 function exhaustiveGuard(value: never): never {
   throw new Error(
@@ -22,22 +24,54 @@ const turretTypes = types.or();
 
 const STARTING_PERIMETER_HEALTH = 50;
 
-const TURRET_LIMIT = 4;
+const EMPLACEMENT_LIMIT = 4;
+
+const EMPLACEMENT_EXPONENT = 1.5;
+
 @model('Perimeter')
 export class Perimeter extends ExtendedModel(ZoneEntity, {
   enemies: tProp(types.array(enemyTypes), () => []),
+  emplacementCount: tProp(types.number, 1),
   turrets: tProp(types.array(turretTypes), () => []),
   perimeterHealth: tProp(types.number, () => STARTING_PERIMETER_HEALTH),
 }) {
   transientUnlockCheck = () => true;
   observableUnlockCheck = () => getRadar(this).unlocked;
 
+  baseCost = [
+    { resource: ResourceNames.ALLOY, quantity: 50 },
+    { resource: ResourceNames.ROCK, quantity: 50 },
+  ];
+
   /**
    * Allowed to puchase more turrets?
    */
   @computed
   get canPurchaseTurret() {
-    return this.turrets.length < TURRET_LIMIT;
+    return this.turrets.length < this.emplacementCount;
+  }
+
+  /**
+   * Cost of next emplacement
+   */
+  @computed
+  get emplacementCost(): PurchaseCost[] {
+    return this.baseCost.map(({ resource, quantity: baseCost }) => {
+      return {
+        resource,
+        quantity: Math.floor(
+          baseCost * EMPLACEMENT_EXPONENT ** this.emplacementCount,
+        ),
+      };
+    });
+  }
+
+  /**
+   * New emplacement available to purchase?
+   */
+  @computed
+  get belowEmplacementLimit() {
+    return this.emplacementCount < EMPLACEMENT_LIMIT && !this.canPurchaseTurret;
   }
 
   /**
