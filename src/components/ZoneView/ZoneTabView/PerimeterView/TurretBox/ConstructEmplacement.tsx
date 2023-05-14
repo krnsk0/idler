53855ef46d90b56199cd2dbfd3e2f59e10825b0e
krnsk0 <jkurinsky@fastmail.com>
2023-05-14 17:10:57 -0400

    more tooltips

--- src/components/ZoneView/ZoneTabView/PerimeterView/TurretBox/ConstructEmplacement.tsx
+++ src/components/ZoneView/ZoneTabView/PerimeterView/TurretBox/ConstructEmplacement.tsx
@@ -2,25 +2,29 @@ import { observer } from 'mobx-react-lite';
 import { styles } from './TurretBox.styles';
 import { Zone } from '../../../../../store/zone/zone';
 import { formatNumber } from '../../../../../utils/formatNumber';
-import { Fragment } from 'react';
+import { Fragment, useRef } from 'react';
+import {
+  DesktopTooltipTitle,
+  TooltipDivider,
+  TooltipPortalRenderer,
+  TooltipText,
+} from '../../../../shared/Tooltip/Tooltip';
 
-function ConstructEmplacement({ zone }: { zone: Zone }) {
-  const costs = zone.perimeter.emplacementCostDisplay;
-  const affordable = zone.perimeter.emplacementAffordable;
-
+const ConstructEmplacementTooltip = observer(({ zone }: { zone: Zone }) => {
   return (
-    <div
-      css={(theme) => [
-        styles.turretBox,
-        styles.constructEmplacementBox(theme, affordable),
-      ]}
-      onClick={() => {
-        zone.perimeter.purchaseEmplacement();
-      }}
-    >
-      <div css={styles.plusSign}>+</div>
-      <div css={styles.costDisplay}>
-        {costs.map(
+    <>
+      <DesktopTooltipTitle showDivider={true}>
+        new emplacement
+      </DesktopTooltipTitle>
+      <TooltipText italic={true} align={'center'}>
+        fortify a new defensive emplacement, allowing the installation of a
+        turret
+      </TooltipText>
+
+      <TooltipDivider text="cost" />
+
+      <TooltipText>
+        {zone.perimeter.emplacementCostDisplay.map(
           ({
             resourceDisplayName,
             quantity,
@@ -30,8 +34,8 @@ function ConstructEmplacement({ zone }: { zone: Zone }) {
           }) => {
             return (
               <Fragment key={resourceDisplayName}>
-                <div css={styles.costHeader}>{resourceDisplayName}</div>
                 <div>
+                  {resourceDisplayName}:{' '}
                   {isSatisfied ? '' : `${formatNumber(availableQuantity)} / `}
                   {formatNumber(quantity, { digits: 0 })}
                   {storageConstrained ? 'ᶜ' : ''}
@@ -40,8 +44,56 @@ function ConstructEmplacement({ zone }: { zone: Zone }) {
             );
           },
         )}
+      </TooltipText>
+    </>
+  );
+});
+
+function ConstructEmplacement({ zone }: { zone: Zone }) {
+  const costs = zone.perimeter.emplacementCostDisplay;
+  const affordable = zone.perimeter.emplacementAffordable;
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <>
+      <TooltipPortalRenderer containerRef={containerRef}>
+        {<ConstructEmplacementTooltip zone={zone} />}
+      </TooltipPortalRenderer>
+      <div
+        css={(theme) => [
+          styles.turretBox,
+          styles.constructEmplacementBox(theme, affordable),
+        ]}
+        onClick={() => {
+          zone.perimeter.purchaseEmplacement();
+        }}
+        ref={containerRef}
+      >
+        <div css={styles.plusSign}>+</div>
+        <div css={styles.costDisplay}>
+          {costs.map(
+            ({
+              resourceDisplayName,
+              quantity,
+              isSatisfied,
+              availableQuantity,
+              storageConstrained,
+            }) => {
+              return (
+                <Fragment key={resourceDisplayName}>
+                  <div css={styles.costHeader}>{resourceDisplayName}</div>
+                  <div>
+                    {isSatisfied ? '' : `${formatNumber(availableQuantity)} / `}
+                    {formatNumber(quantity, { digits: 0 })}
+                    {storageConstrained ? 'ᶜ' : ''}
+                  </div>
+                </Fragment>
+              );
+            },
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/ZoneView/ZoneTabView/PerimeterView/TurretBox/SelectTurret.tsx
+++ src/components/ZoneView/ZoneTabView/PerimeterView/TurretBox/SelectTurret.tsx
@@ -1,19 +1,37 @@
 import { observer } from 'mobx-react-lite';
 import { styles } from './TurretBox.styles';
 import { Zone } from '../../../../../store/zone/zone';
+import { useRef } from 'react';
+import {
+  DesktopTooltipTitle,
+  TooltipPortalRenderer,
+  TooltipText,
+} from '../../../../shared/Tooltip/Tooltip';
 
 function SelectTurret({ zone }: { zone: Zone }) {
   const turretIndex = zone.perimeter.turrets.length;
+  const containerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div
-      css={[styles.turretBox, styles.emptyTurretBox]}
-      onClick={() => zone.perimeter.openTurretPurchaseModal(turretIndex)}
-    >
-      <div css={styles.plusSign}>+</div>
-      <div>install</div>
-      <div>turret</div>
-    </div>
+    <>
+      <TooltipPortalRenderer containerRef={containerRef}>
+        <DesktopTooltipTitle showDivider={true}>
+          install new turret
+        </DesktopTooltipTitle>
+        <TooltipText italic={true} align={'center'}>
+          select a new turret to install atop this defensive emplacement
+        </TooltipText>
+      </TooltipPortalRenderer>
+      <div
+        css={[styles.turretBox, styles.emptyTurretBox]}
+        onClick={() => zone.perimeter.openTurretPurchaseModal(turretIndex)}
+        ref={containerRef}
+      >
+        <div css={styles.plusSign}>+</div>
+        <div>install</div>
+        <div>turret</div>
+      </div>
+    </>
   );
 }
 
